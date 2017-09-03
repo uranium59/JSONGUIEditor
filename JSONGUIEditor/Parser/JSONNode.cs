a78f691b9e197de39a791709736e582d3c2a1fe5
NukeGenerator\Uranium <gdzergling@core-a.org>
2017-09-04 00:21:23 +0900

    첫 커밋.

---
+++ JSONGUIEditor/Parser/JSONNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONGUIEditor.Parser
+{
+    public class JSONNode : IJSON
+    {
+        public JSONNode()
+        {
+
+        }
+
+        public JSONNode(string str)
+        {
+
+        }
+    }
+}
---
+++ JSONGUIEditor/Parser/State/JSONType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONGUIEditor.Parser.State
+{
+    public enum JSONType : int
+    {
+        Array = 0,
+        Bool,
+        Object,
+        Number,
+        Null,
+        String,
+    }
+}
