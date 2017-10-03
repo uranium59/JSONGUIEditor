5187fff75bd0cb949216684945a80529b931c9f9
uranium59 <gdzergling@core-a.org>
2017-10-03 22:00:49 +0900

    start real parsing

--- JSONGUIEditor/Parser/JSONParseThread.cs
+++ JSONGUIEditor/Parser/JSONParseThread.cs
@@ -8,5 +8,25 @@ namespace JSONGUIEditor.Parser
 {
     class JSONParseThread
     {
+        //if complexity is bigger than threshold, add thread into threadpool
+        static public const int ComplexityThreshold = 10;
+        static public const bool UsingThread = true;
+        
+        static public JSONNode Parse(MyTree<int, object> t, string s)
+        {
+            foreach(MyTree<int, object> c in t)
+            {
+                if(c.Complex > ComplexityThreshold)
+                {
+                }
+            }
+        }//for single thread
+        
+        static public JSONNode ParseThread(MyTree<int, object> t, string s)
+        {
+            if(c.Complex > ComplexityThreshold)
+            {
+            }
+        }//for multithread
     }
 }
